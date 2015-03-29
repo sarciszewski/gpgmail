ab3ad779138799d0a7ccd35f714ab06763fe7408
Scott Arciszewski <scott@arciszewski.me>
2015-03-28 20:57:01 -0400

    Spaces not tabs

--- src/public/contact_form.php
+++ src/public/contact_form.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>GPG Mailer</title>
-	</head>
-	<body>
+    <head>
+        <title>GPG Mailer</title>
+    </head>
+    <body>
 <?php
 
 ?>
-		<form action="contact_form.php" method="post">
-			Your Email Address:
-			<input type="text" name="from" placeholder="example@something.com" /><br />
-			
-			Your message:<br />
-			<textarea name="message" placeholder="Your message will be encrypted in transit."></textarea>
-			<button type="submit"> Fire Away! </button>			
-		</form>
-	</body>
+        <form action="contact_form.php" method="post">
+            Your Email Address:
+            <input type="text" name="from" placeholder="example@something.com" /><br />
+
+            Your message:<br />
+            <textarea name="message" placeholder="Your message will be encrypted in transit."></textarea>
+            <button type="submit"> Fire Away! </button>
+        </form>
+    </body>
 </html>
--- src/public/contact_us.php
+++ src/public/contact_us.php
@@ -8,26 +8,26 @@ require BASE.'/config.php';
 
 
 if (isset($_POST['from']) && isset($_POST['message'])) {
-	$gpg = new Crypt_GPG([
+    $gpg = new Crypt_GPG([
         'homedir' => BASE
     ]);
     $fingerprint = $gpg->getFingerprint(
         OUR_EMAIL_ADDRESS
     );
-	
-	$ciphertext = $gpg->encrypt(
-		"From: {$_POST['from']}\n\nMessage:\n{$_POST['message']}",
+
+    $ciphertext = $gpg->encrypt(
+        "From: {$_POST['from']}\n\nMessage:\n{$_POST['message']}",
         $fingerprint
-	);
-	
-	$email = Mail::factory('mail');
-	$email->send(
-		[OUR_EMAIL_ADDRESS], // Just for the lulz
-		['From' => 'Contact Form <contact@anonymo.us>'],
-		$ciphertext
-	);
-	header("Location: contact_form.php?msg=success");
-	exit;
+    );
+
+    $email = Mail::factory('mail');
+    $email->send(
+        [OUR_EMAIL_ADDRESS], // Just for the lulz
+        ['From' => 'Contact Form <contact@anonymo.us>'],
+        $ciphertext
+    );
+    header("Location: contact_form.php?msg=success");
+    exit;
 }
 header("Location: contact_form.php");
 exit;
